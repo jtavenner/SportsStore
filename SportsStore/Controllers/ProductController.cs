bd5de6effc89306e8b7e17ec0f1f211b52d773e2
jtavenner <username@users.noreply.github.com>
2018-09-16 18:18:02 -0400

    Corected Paging

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -28,7 +28,9 @@ namespace SportsStore.Controllers
                 {
                     CurrentPage = productPage,
                     ItemsPerpage = PageSize,
-                    TotalItems = repository.Products.Count()
+                    TotalItems = category == null ?
+                        repository.Products.Count() :
+                        repository.Products.Where(e => e.Category == category).Count()
                 },
                 CurrentCategory = category
             });
